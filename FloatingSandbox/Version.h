ec0f92b80f514992d3b76854fd8f36b79cb590c8
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2019-01-29 20:57:04 +0100

    Working version info

--- FloatingSandbox/Version.h
+++ FloatingSandbox/Version.h
@@ -8,9 +8,26 @@
 #include <cassert>
 #include <string>
 
-#define VERSION "1.8.0"
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
 
-inline std::string ApplicationName = "Floating Sandbox";
+#define VERSION_MAJOR               1
+#define VERSION_MINOR               8
+#define VERSION_REVISION            0
+#define VERSION_BUILD               0
+
+// TODOHERE: APPLICATION_VERSION_STR, APPLICATION_NAME, APPLICATION_NAME_WITH_VERSION
+#define VER_PRODUCT_VERSION_STR     STRINGIZE(VERSION_MAJOR)        \
+                                    "." STRINGIZE(VERSION_MINOR)    \
+                                    "." STRINGIZE(VERSION_REVISION) \
+                                    "." STRINGIZE(VERSION_BUILD)    \
+
+#define VER_PRODUCTNAME_STR                 "Floating Sandbox"
+#define VER_PRODUCTNAME_WITH_VERSION_STR    VER_PRODUCTNAME_STR " " VER_PRODUCT_VERSION_STR
+
+#define VERSION STRINGIZE(VERSION_MAJOR) "." STRINGIZE(VERSION_MINOR) "." STRINGIZE(VERSION_REVISION)
+
+inline std::string ApplicationName = VER_PRODUCTNAME_STR;
 
 enum class VersionFormat
 {
