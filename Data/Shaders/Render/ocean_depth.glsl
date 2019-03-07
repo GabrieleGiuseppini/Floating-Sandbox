599e65b7a256be8373025c869dac7ca2bd47bb11
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2019-03-07 19:38:47 +0100

    Fixed issue with water gradient rendering

--- Data/Shaders/Render/ocean_depth.glsl
+++ Data/Shaders/Render/ocean_depth.glsl
@@ -21,12 +21,9 @@ out vec4 oceanColor;
 void main()
 {
     // Calculate color
-    oceanColor = 
-        vec4(paramOceanDepthColorStart.xyz, 1.0 - paramOceanTransparency) * (1 - inOceanAttribute.z)
-        + vec4(paramOceanDepthColorEnd.xyz, 1.0 - paramOceanTransparency) * inOceanAttribute.z;
-
-    // Apply ambient light intensity
-    oceanColor = oceanColor * paramAmbientLightIntensity;
+    vec3 oceanColorTmp = paramOceanDepthColorStart * (1 - inOceanAttribute.z)
+        + paramOceanDepthColorEnd * inOceanAttribute.z;
+    oceanColor = vec4(oceanColorTmp.xyz * paramAmbientLightIntensity, 1.0 - paramOceanTransparency);
 
     // Calculate position
     gl_Position = paramOrthoMatrix * vec4(inOceanAttribute.xy, -1.0, 1.0);
