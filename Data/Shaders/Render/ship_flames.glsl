a5dfa50fbdace3927c7b7b32606543df2db64a7e
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2019-06-12 21:55:43 +0200

    Fixed NDC transformation

--- Data/Shaders/Render/ship_flames.glsl
+++ Data/Shaders/Render/ship_flames.glsl
@@ -37,13 +37,24 @@ uniform float paramTime;
 uniform vec2 paramViewportSize;
 uniform float paramWindSpeedMagnitude;
 
+mat2 GetRotationMatrix(float angle)
+{
+    mat2 m;
+    m[0][0] = cos(angle); m[0][1] = -sin(angle);
+    m[1][0] = sin(angle); m[1][1] = cos(angle);
+    return m;
+}
+
+#define FlameSpeed 0.17
+#define NoiseResolution 0.4
+
 void main()
 {
-    // Calculate fragment's coordinates in the NDC space
-    vec2 ndc = (gl_FragCoord.xy / paramViewportSize.xy) * 2.0 - vec2(1.0);
+    // Calculate fragment's coordinates in the pseudo-NDC space (-0.5, +0.5)
+    vec2 ndc = (gl_FragCoord.xy / paramViewportSize.xy) - vec2(0.5);
 
     // Center and flip vertically
-    ndc = vec2(ndc.x - vertexCenterPosition.x, vertexCenterPosition.y - ndc.y);
+    ndc = vec2(ndc.x - vertexCenterPosition.x / 2.0, vertexCenterPosition.y / 2.0 - ndc.y);
 
     // Adjust for aspect ratio
     ndc.y /= (paramViewportSize.x / paramViewportSize.y);
@@ -52,6 +63,6 @@ void main()
     float d = length(ndc);
     d = d + paramTime + paramWindSpeedMagnitude;
     d = d - paramTime - paramWindSpeedMagnitude;
-    vec3 col = mix(vec3(0.8, 0.1, 0.1), vec3(0.1, 0.1, 0.8), 1.0 - smoothstep(0.0, 0.1, d));
+    vec3 col = mix(vec3(0.8, 0.1, 0.1), vec3(0.1, 0.1, 0.8), 1.0 - smoothstep(0.0, 0.05, d));
     gl_FragColor = vec4(col.xyz, 1.0);
 } 
