b04c80bbf780c3680872da3278583b36a33cc347
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2019-06-22 00:45:59 +0200

    Awesome repair params; cleaned up experimental code

--- Game/Ship_Interactions.cpp
+++ Game/Ship_Interactions.cpp
@@ -268,7 +268,7 @@ void Ship::RepairAt(
     // required for repairing
     // TODOTEST
     //float constexpr SmoothingAlpha = 0.03f;
-    float constexpr SmoothingAlpha = 0.001f;
+    float constexpr SmoothingAlpha = 0.0015f;
 
     // Tolerance to distance
     //
@@ -342,104 +342,6 @@ void Ship::RepairAt(
                 mPoints.GetRepairState(pointIndex).SmoothingSessionStepId = sessionStepId;
             }
 
-
-
-            //
-            // TODO) EXPERIMENTAL: Detect springs in wrong positions
-            //
-
-            if (mPoints.GetConnectedSprings(pointIndex).ConnectedSprings.size() >= 3)
-            {
-                //
-                // Count number of springs in correct and wrong positions
-                //
-
-                int nWrongCount = 0;
-                int nCorrectCount = 0;
-
-                for (size_t s = 0; s < mPoints.GetConnectedSprings(pointIndex).ConnectedSprings.size(); ++s)
-                {
-                    ElementIndex const sIndex = mPoints.GetConnectedSprings(pointIndex).ConnectedSprings[s].SpringIndex;
-
-                    // Ignore standalone springs
-                    if (mPoints.GetConnectedSprings(mSprings.GetOtherEndpointIndex(sIndex, pointIndex)).ConnectedSprings.size() < 2)
-                        continue;
-
-                    int const sFactoryOctant = mSprings.GetFactoryEndpointOctant(sIndex, pointIndex);
-
-                    // Find the next spring in world CW order
-                    ElementIndex sNextSpringIndexWorld = std::numeric_limits<ElementIndex>::max();
-                    float sMinAngle = std::numeric_limits<float>::max();
-                    for (size_t s2 = 0; s2 < mPoints.GetConnectedSprings(pointIndex).ConnectedSprings.size(); ++s2)
-                    {
-                        if (s2 == s)
-                            continue;
-
-                        ElementIndex const s2Index = mPoints.GetConnectedSprings(pointIndex).ConnectedSprings[s2].SpringIndex;
-
-                        // Ignore standalone springs
-                        if (mPoints.GetConnectedSprings(mSprings.GetOtherEndpointIndex(s2Index, pointIndex)).ConnectedSprings.size() < 2)
-                            continue;
-
-                        float angle =
-                            (mPoints.GetPosition(mSprings.GetOtherEndpointIndex(sIndex, pointIndex)) - mPoints.GetPosition(pointIndex))
-                            .angle(mPoints.GetPosition(mSprings.GetOtherEndpointIndex(s2Index, pointIndex)) - mPoints.GetPosition(pointIndex));
-
-                        if (angle < 0.0f)
-                            angle += 2.0f * Pi<float>;
-
-                        if (angle < sMinAngle)
-                        {
-                            sNextSpringIndexWorld = s2Index;
-                            sMinAngle = angle;
-                        }
-                    }
-
-                    if (sNextSpringIndexWorld == std::numeric_limits<ElementIndex>::max())
-                        continue;
-
-                    // Find the next spring in factory octant CW order
-                    ElementIndex sNextSpringIndexFactoryOctant = std::numeric_limits<ElementIndex>::max();
-                    int sMinOctantDelta = std::numeric_limits<int>::max();
-                    for (size_t s2 = 0; s2 < mPoints.GetConnectedSprings(pointIndex).ConnectedSprings.size(); ++s2)
-                    {
-                        if (s2 == s)
-                            continue;
-
-                        ElementIndex const s2Index = mPoints.GetConnectedSprings(pointIndex).ConnectedSprings[s2].SpringIndex;
-
-                        // Ignore standalone springs
-                        if (mPoints.GetConnectedSprings(mSprings.GetOtherEndpointIndex(s2Index, pointIndex)).ConnectedSprings.size() < 2)
-                            continue;
-
-                        int cwDelta =
-                            mSprings.GetFactoryEndpointOctant(s2Index, pointIndex)
-                            - sFactoryOctant;
-
-                        if (cwDelta < 0)
-                            cwDelta += 8;
-
-                        if (cwDelta < sMinOctantDelta)
-                        {
-                            sNextSpringIndexFactoryOctant = s2Index;
-                            sMinOctantDelta = cwDelta;
-                        }
-                    }
-
-                    assert(sNextSpringIndexFactoryOctant != std::numeric_limits<ElementIndex>::max());
-
-                    // Calculate right/wrong counts
-                    if (sNextSpringIndexWorld == sNextSpringIndexFactoryOctant)
-                        ++nCorrectCount;
-                    else
-                        ++nWrongCount;
-                }
-
-                if (nWrongCount != 0)
-                {
-                    LogMessage("Point: ", pointIndex, " C=", nCorrectCount, " W=", nWrongCount);
-                }
-            }
 
 
             //
@@ -590,11 +492,6 @@ void Ship::RepairAt(
                             + vec2f::fromPolar(
                                 mSprings.GetRestLength(fcs.SpringIndex),
                                 targetWorldAngle);
-
-                        // TODOTEST
-                        ////LogMessage(pointIndex, "(@", mPoints.GetPosition(pointIndex).toString(), ") attracts ",
-                        ////    otherEndpointIndex, "(@", mPoints.GetPosition(otherEndpointIndex), ") towards ",
-                        ////    targetOtherEndpointPosition.toString());
 
 
                         //
