ae359b276ac940f1e35af67db3c745073cccca17
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2019-10-15 22:26:57 +0200

    Created storm

---
+++ Game/Storm.cpp
@@ -0,0 +1,24 @@
+﻿/***************************************************************************************
+* Original Author:		Gabriele Giuseppini
+* Created:				2019-10-15
+* Copyright:			Gabriele Giuseppini  (https://github.com/GabrieleGiuseppini)
+***************************************************************************************/
+#include "Physics.h"
+
+#include <GameCore/GameRandomEngine.h>
+
+namespace Physics {
+
+void Storm::Update(
+    float currentSimulationTime,
+    GameParameters const & gameParameters)
+{
+    // TODO
+}
+
+void Storm::Upload(Render::RenderContext & renderContext) const
+{
+    // TODO
+}
+
+}
---
+++ Game/Storm.h
@@ -0,0 +1,45 @@
+/***************************************************************************************
+* Original Author:		Gabriele Giuseppini
+* Created:				2019-10-15
+* Copyright:			Gabriele Giuseppini  (https://github.com/GabrieleGiuseppini)
+***************************************************************************************/
+#pragma once
+
+#include "GameParameters.h"
+#include "RenderContext.h"
+
+namespace Physics
+{
+
+class Storm
+{
+
+public:
+
+    Storm()
+        : mParameters()
+    {}
+
+    void Update(
+        float currentSimulationTime,
+        GameParameters const & gameParameters);
+
+    void Upload(Render::RenderContext & renderContext) const;
+
+public:
+
+    struct Parameters
+    {
+    };
+
+    Parameters const & GetParameters() const
+    {
+        return mParameters;
+    }
+
+private:
+
+    Parameters mParameters;
+};
+
+}
