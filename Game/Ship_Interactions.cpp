45f7c19f8708bd408ceae5080230ec72752b7248
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2020-07-16 20:57:21 +0200

    Fixed compilation issue

--- Game/Ship_Interactions.cpp
+++ Game/Ship_Interactions.cpp
@@ -1338,7 +1338,7 @@ std::optional<vec2f> Ship::FindSuitableLightningTarget() const
                         {
                             // Height of to-be-inserted point is augmented based on distance from the point
                             float const distance = (candidatePos - pos).length();
-                            float const actualPosY = pos.y + std::max(floor(distance / 3.0f), 5.0f);
+                            float const actualPosY = pos.y + std::max(std::floor(distance / 3.0f), 5.0f);
                             return candidatePos.y > actualPosY;
                         }),
                     pos);
