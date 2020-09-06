132ab643dc202eeac7fe924764eb05b015ecca0d
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2020-09-06 02:21:16 +0200

    Fixed tsunami rate

--- Game/GameParameters.cpp
+++ Game/GameParameters.cpp
@@ -37,7 +37,7 @@ GameParameters::GameParameters()
     , BasalWaveHeightAdjustment(1.0f)
     , BasalWaveLengthAdjustment(1.0f)
     , BasalWaveSpeedAdjustment(4.0f)
-    , TsunamiRate(10)
+    , TsunamiRate(12)
     , RogueWaveRate(2)
     // Storm
 	, StormRate(60)
