524b1301d1a2a7035ec988b06cae5830da710aeb
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2021-05-24 21:15:04 +0200

    More EventTicker debugging

--- FloatingSandbox/EventTickerPanel.h
+++ FloatingSandbox/EventTickerPanel.h
@@ -127,9 +127,9 @@ private:
     // the left. This string might be empty.
     std::string mFutureTickerText;
 
-    // The width of a character
-    unsigned int mCharWidth;
+    // The size of a character
+    wxSize mCharSize;
 
     // The fraction of the character width that we're currently scrolled by
-    unsigned int mCurrentCharWidthStep;
+    int mCurrentCharWidthStep;
 };
