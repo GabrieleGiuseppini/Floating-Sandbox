0907739b45c9782a5d24c6bacc2127e6a43f9820
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2022-02-11 19:26:53 +0100

    Undone maybe_unused hack

--- GameCore/SysSpecifics.h
+++ GameCore/SysSpecifics.h
@@ -85,10 +85,6 @@ using register_int = register_int_64;
 
 #define restrict __restrict
 
-#ifdef __GNUC__
-#define [[maybe_unused]]
-#endif
-
 template<typename T>
 inline constexpr T ceil_power_of_two(T value)
 {
