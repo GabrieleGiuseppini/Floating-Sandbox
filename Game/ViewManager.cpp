6c7f46c4707bfe0078cf81f4bca72631ad966e68
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2022-04-25 00:48:57 +0200

    Using thresholds

--- Game/ViewManager.cpp
+++ Game/ViewManager.cpp
@@ -154,39 +154,70 @@ void ViewManager::Update(Geometry::AABBSet const & allAABBs)
 
             float constexpr NdcFractionZoomTarget = 0.5f; // Fraction of the [0, 2] NDC space that needs to be occupied by AABB
 
+            //float constexpr ZoomThreshold = 0.1f;
+            float constexpr ZoomThreshold = 0.0f;
+
             // Calculate NDC extent of AABB's extent at current ViewModel params [0, 2]
             vec2f const aabbNdcExtent = mRenderContext.WorldOffsetToNdc(
                 vec2f(unionAABB->GetWidth(), unionAABB->GetHeight()),
                 mZoomParameterSmoother->GetValue());
 
             // Calculate needed zoom to fit in fraction of NDC space - choosing the furthest among the two dimensions
+            float const visibleWorldWidth = mRenderContext.CalculateVisibleWorldWidth(mZoomParameterSmoother->GetValue());
+            float const visibleWorldHeight = mRenderContext.CalculateVisibleWorldHeight(mZoomParameterSmoother->GetValue());
             float const autoFocusZoom = std::min(
-                mRenderContext.CalculateZoomForNdcWidth(aabbNdcExtent.x / NdcFractionZoomTarget),
-                mRenderContext.CalculateZoomForNdcHeight(aabbNdcExtent.y / NdcFractionZoomTarget));
+                mRenderContext.CalculateZoomForWorldWidth(aabbNdcExtent.x / NdcFractionZoomTarget * visibleWorldWidth / 2.0f),
+                mRenderContext.CalculateZoomForWorldHeight(aabbNdcExtent.y / NdcFractionZoomTarget * visibleWorldHeight / 2.0f));
 
-            // Set zoom
-            // TODOHERE: threshold check
-            mZoomParameterSmoother->SetValue(autoFocusZoom);
+            // Set zoom, with threshold check
+
+            float newAutoFocusZoom;
+            float const oldZoom = mZoomParameterSmoother->GetValue();
+            if (std::abs(autoFocusZoom - oldZoom) > ZoomThreshold)
+            {
+                // Apply auto-focus
+                newAutoFocusZoom = autoFocusZoom;
+            }
+            else
+            {
+                // Stay
+                newAutoFocusZoom = oldZoom;
+            }
+            
+            mZoomParameterSmoother->SetValue(newAutoFocusZoom);
 
             //
             // Pan
             //
+
+            //float constexpr CameraPositionThreshold = 30.0f; // Attempt to avoid panning with waves
+            float constexpr CameraPositionThreshold = 0.0f;
             
             // Calculate AABB in NDC coordinates, using the target zoom
             vec2f const aabbNdcBottomLeft = mRenderContext.WorldToNdc(unionAABB->BottomLeft, mZoomParameterSmoother->GetValue(), mCameraWorldPositionParameterSmoother->GetValue());
             vec2f const aabbNdcTopRight = mRenderContext.WorldToNdc(unionAABB->TopRight, mZoomParameterSmoother->GetValue(), mCameraWorldPositionParameterSmoother->GetValue());
 
             // Calculate world offset required to center view onto AABB's center
-            vec2f const offsetWorld = vec2f(
+            vec2f const autoFocusCameraWorldPositionOffset = vec2f(
                 (aabbNdcBottomLeft.x + aabbNdcTopRight.x) / 2.0f * mRenderContext.CalculateVisibleWorldWidth(mZoomParameterSmoother->GetValue()) / 2.0f,
                 (aabbNdcBottomLeft.y + aabbNdcTopRight.y) / 2.0f * mRenderContext.CalculateVisibleWorldHeight(mZoomParameterSmoother->GetValue()) / 2.0f);
 
-            // Set camera offset
-            // TODOHERE: threshold check
-            vec2f const newTargetCameraWorldPosition =
-                mCameraWorldPositionParameterSmoother->GetValue()
-                + offsetWorld;
-            mCameraWorldPositionParameterSmoother->SetValue(newTargetCameraWorldPosition);
+            // Set camera position, with threshold check
+
+            vec2f newAutoFocusCameraWorldPosition;
+            if (std::abs(autoFocusCameraWorldPositionOffset.x) > CameraPositionThreshold
+                || std::abs(autoFocusCameraWorldPositionOffset.y) > CameraPositionThreshold)
+            {
+                // Apply auto-focus
+                newAutoFocusCameraWorldPosition = mCameraWorldPositionParameterSmoother->GetValue() + autoFocusCameraWorldPositionOffset;
+            }
+            else
+            {
+                // Stay
+                newAutoFocusCameraWorldPosition = mCameraWorldPositionParameterSmoother->GetValue();
+            }
+            
+            mCameraWorldPositionParameterSmoother->SetValue(newAutoFocusCameraWorldPosition);
             
 
             /* TODOOLD
