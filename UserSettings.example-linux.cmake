709e75272c86948891a6b1450437cf455bad8264
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2024-03-22 13:19:38 +0100

    Picking up SFML as dynlibs

--- UserSettings.example-linux.cmake
+++ UserSettings.example-linux.cmake
@@ -6,7 +6,6 @@ set(SDK_ROOT "/home/gg/fs_libs")
 set(REPOS_ROOT "/home/gg/git")
 
 set(DevIL_ROOT_DIR "${SDK_ROOT}/DevIL")
-set(SFML_ROOT "${SDK_ROOT}/SFML/lib/cmake/SFML")
 set(BENCHMARK_ROOT_DIR "${SDK_ROOT}/benchmark")
 set(wxWidgets_ROOT "${SDK_ROOT}/wxWidgets") # Only works with CMake >= 3.12 - directs FindwxWidgets.cmake to the right root
 set(GTEST_DIR "${REPOS_ROOT}/googletest")
