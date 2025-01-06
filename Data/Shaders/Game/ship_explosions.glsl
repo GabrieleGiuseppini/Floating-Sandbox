dd8ef8eeebf4d7653f49ac3025f686b3023b6a15
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2025-01-06 23:38:23 +0100

    Fine-tuning

--- Data/Shaders/Game/ship_explosions.glsl
+++ Data/Shaders/Game/ship_explosions.glsl
@@ -4,6 +4,16 @@
 #define EXPLOSION_TYPE_FIRE_EXTINGUISHING 2.0
 #define EXPLOSION_TYPE_SODIUM 3.0
 
+mat2 get_rotation_matrix(float angle)
+{
+    mat2 m;
+    m[0][0] = cos(angle); m[0][1] = -sin(angle);
+    m[1][0] = sin(angle); m[1][1] = cos(angle);
+
+    return m;
+}
+
+
 ###VERTEX-120
 
 #define in attribute
@@ -25,19 +35,17 @@ uniform mat4 paramOrthoMatrix;
 
 void main()
 {
-    vertexTextureCoordinates = inExplosion2.xy; 
+    vertexTextureCoordinates = inExplosion2.xy;
     vertexExplosionIndex = inExplosion3.x;
     vertexExplosionType = inExplosion3.y;
     vertexProgress = inExplosion3.z;
 
     float angle = inExplosion2.w;
 
-    mat2 rotationMatrix = mat2(
-        cos(angle), -sin(angle),
-        sin(angle), cos(angle));
+    mat2 rotationMatrix = get_rotation_matrix(angle);
 
-    vec2 worldPosition = 
-        inExplosion1.xy 
+    vec2 worldPosition =
+        inExplosion1.xy
         + rotationMatrix * inExplosion1.zw;
 
     gl_Position = paramOrthoMatrix * vec4(worldPosition.xy, inExplosion2.z, 1.0);
@@ -75,17 +83,8 @@ float is_type(float notification_type, float value)
     return step(value - 0.5, notification_type) * step(notification_type, value + 0.5);
 }
 
-mat2 get_rotation_matrix(float angle)
-{
-    mat2 m;
-    m[0][0] = cos(angle); m[0][1] = -sin(angle);
-    m[1][0] = sin(angle); m[1][1] = cos(angle);
-
-    return m;
-}
-
 vec4 sample_texture(float frameIndex, vec2 uv)
-{   
+{
     // Row at which the desired explosion starts
     float explosionIndexRowStart = 2. * vertexExplosionIndex;
 
@@ -95,15 +94,15 @@ vec4 sample_texture(float frameIndex, vec2 uv)
     // Coords on frame grid having integral indices,
     // clamped to the two rows of this explosion index
     vec2 atlasCoords = vec2(
-        c + uv.x, 
+        c + uv.x,
         clamp(
             r + uv.y,
             explosionIndexRowStart, // Assuming here it's alpha==0 for every x
             explosionIndexRowStart + 2.));
-    
+
     // Transform into texture coords
     vec2 atlasSampleCoords = atlasCoords * FrameSideSize;
-    
+
     // Sample
     return texture2D(paramExplosionsAtlasTexture, atlasSampleCoords);
 }
@@ -115,25 +114,25 @@ void main()
     // - Frame 1: index=(bucket - 1.), alpha=(1. - inBucket) (1.0 -> 0.0), scale=(1. + Delta * inBucket) (1.0 -> 1.0+Delta)
     // - Frame 2: index=(bucket), alpha=(inBucket) (0.0 -> 1.0), scale= (1. + Delta * inBucket - Delta) (1.0-Delta -> 1.0)
     //
-    
+
     float sigma = 1./(NExplosionFrames + 1.);
     float bucket = floor(vertexProgress / sigma);
     float inBucket = fract(vertexProgress / sigma);
-            
-    
+
+
     //
     // Scale
     //
-    
+
     #define ScaleDelta .16
     float scale1 = 1. + ScaleDelta * inBucket;
-    float scale2 = scale1 - ScaleDelta;       
-    
-    
+    float scale2 = scale1 - ScaleDelta;
+
+
     //
     // Sample
     //
-    
+
     vec2 centeredSpacePosition = vertexTextureCoordinates - vec2(.5);
     vec4 c1 = sample_texture(bucket - 1., centeredSpacePosition / scale1 + vec2(.5));
     vec4 c2 = sample_texture(bucket, centeredSpacePosition / scale2 + vec2(.5));
@@ -148,56 +147,57 @@ void main()
     vec4 fire_extinguishing_color;
     {
         #define SpraySpeed 0.3
-        float sprayTime = vertexProgress * SpraySpeed;    
+        float sprayTime = vertexProgress * SpraySpeed;
 
         // Polar coords
         centeredSpacePosition *= 2.0;
         float polarRadius = length(centeredSpacePosition);
         float polarAngle = atan(centeredSpacePosition.y, centeredSpacePosition.x) / (2.0 * PI);
-    
+
         // Rotate based on noise sampled via polar coordinates of pixel
-        #define AngleNoiseResolution 1.0        
-        vec2 noiseSampleCoords = 
+        #define AngleNoiseResolution 0.8
+        vec2 noiseSampleCoords =
             vec2(polarRadius, polarAngle + 0.5) * AngleNoiseResolution
             + vec2(-0.5 * sprayTime, 0.2* sprayTime);
 
         float angle = texture2D(paramNoiseTexture, noiseSampleCoords).r; // 0.0 -> 1.0
         // Magnify rotation amount based on distance from center
         angle *= 1.54 * smoothstep(0.0, 0.6, polarRadius);
-        
+
         // Rotate!
         centeredSpacePosition += get_rotation_matrix(angle) * centeredSpacePosition;
-        
+
         //
         // Transform to polar coordinates
         //
-        
-        // (r, a) (r=[0.0, 1.0], a=[0.0, 1.0 CCW from W])
-            
-        vec2 ra = vec2(
-            polarRadius, 
-            (atan(centeredSpacePosition.y, centeredSpacePosition.x) / (2.0 * PI) + 0.5));
-            
-        // Scale radius to better fit in quad
-        ra.x *= 1.7;                   
 
-        
+        // (r, a) (r=[0.0, 1.0], a=[0.0, 1.0 CCW from W])
+
+        vec2 ra = vec2(
+            polarRadius,
+            (atan(centeredSpacePosition.y, centeredSpacePosition.x) / (2.0 * PI) + 0.5));
+
+        // Scale radius to better fit in quad
+        ra.x *= 1.7;
+
+
         //
         // Randomize radius based on noise and radius
         //
-        
+
         #define VariationRNoiseResolution 1.0
         float variationR = texture2D(
-            paramNoiseTexture, 
-            ra * vec2(VariationRNoiseResolution / 4.0, VariationRNoiseResolution / 1.0) 
+            paramNoiseTexture,
+            ra * vec2(VariationRNoiseResolution / 4.0, VariationRNoiseResolution / 1.0)
             + vec2(-sprayTime, 0.0)).r;
-        
+
         variationR -= 0.5;
 
         // Straighten the spray at the center and make full turbulence outside,
         // scaling it at the same time
         variationR *= 0.35 * smoothstep(-0.4, 0.4, ra.x);
 
+        // Randomize!
         float radius = ra.x + variationR;
 
         // Focus (compress dynamics)
@@ -226,4 +226,4 @@ void main()
         c * is_type(vertexExplosionType, EXPLOSION_TYPE_DEFAULT)
         + fire_extinguishing_color * is_type(vertexExplosionType, EXPLOSION_TYPE_FIRE_EXTINGUISHING)
         + sodium_color * is_type(vertexExplosionType, EXPLOSION_TYPE_SODIUM);
-} 
+}
