b4f0376e6f602bcdd2894bc399484ea8c089d9bf
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2025-04-23 18:42:22 +0200

    More unit tests

--- Sources/UnitTests/ProgressCallbackTests.cpp
+++ Sources/UnitTests/ProgressCallbackTests.cpp
@@ -145,7 +145,7 @@ TEST_F(ProgressCallbackTests, ProgressCallback_Direct_WithRange_SubCallback_Twic
     EXPECT_NEAR(mProgressCalls[2], Pc3Left + 1.0f * 0.8f * 0.2f * 0.4f, 0.0001f);
 }
 
-TEST_F(ProgressCallbackTests, ProgressCallback_Direct_WithRange_SubCallback_Cloned)
+TEST_F(ProgressCallbackTests, ProgressCallback_Direct_WithRange_SubCallback_Twice_ThenCloned)
 {
     ProgressCallback pc = ProgressCallback(
         [&](float progress, ProgressMessageType message)
@@ -181,4 +181,58 @@ TEST_F(ProgressCallbackTests, ProgressCallback_Direct_WithRange_SubCallback_Clon
     EXPECT_NEAR(mProgressCalls[0], Pc3Left, 0.0001f);
     EXPECT_NEAR(mProgressCalls[1], Pc3Left + 0.4f * 0.8f * 0.2f * 0.4f, 0.0001f);
     EXPECT_NEAR(mProgressCalls[2], Pc3Left + 1.0f * 0.8f * 0.2f * 0.4f, 0.0001f);
+}
+
+TEST_F(ProgressCallbackTests, SimpleCallback_Direct_SubCallback_Once)
+{
+    SimpleProgressCallback sc = SimpleProgressCallback(
+        [&](float progress)
+        {
+            mProgressCalls.push_back(progress);
+        });
+
+    SimpleProgressCallback sc2 = sc.MakeSubCallback(0.2f, 0.4f);
+
+    // Test
+
+    sc2(0.0f);
+    sc2(0.4f);
+    sc2(1.0f);
+
+    // Verify
+
+    ASSERT_EQ(mProgressCalls.size(), 3u);
+
+    EXPECT_NEAR(mProgressCalls[0], 0.2f, 0.0001f);
+    EXPECT_NEAR(mProgressCalls[1], 0.2f + 0.4f * 0.4f, 0.0001f);
+    EXPECT_NEAR(mProgressCalls[2], 0.6f, 0.0001f);
+}
+
+TEST_F(ProgressCallbackTests, SimpleCallback_Direct_SubCallback_Twice)
+{
+    SimpleProgressCallback sc = SimpleProgressCallback(
+        [&](float progress)
+        {
+            mProgressCalls.push_back(progress);
+        });
+
+    SimpleProgressCallback sc2 = sc.MakeSubCallback(0.2f, 0.4f);
+
+    SimpleProgressCallback sc3 = sc2.MakeSubCallback(0.1f, 0.8f);
+
+    // Test
+
+    sc3(0.0f);
+    sc3(0.4f);
+    sc3(1.0f);
+
+    // Verify
+
+    ASSERT_EQ(mProgressCalls.size(), 3u);
+
+    float constexpr Pc2Left = 0.2f;
+    float constexpr Pc3Left = Pc2Left + 0.1f * 0.4f;
+    EXPECT_NEAR(mProgressCalls[0], Pc3Left, 0.0001f);
+    EXPECT_NEAR(mProgressCalls[1], Pc3Left + 0.4f * 0.8f * 0.4f, 0.0001f);
+    EXPECT_NEAR(mProgressCalls[2], Pc3Left + 1.0f * 0.8f * 0.4f, 0.0001f);
 }
