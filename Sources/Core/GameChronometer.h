23d8474e5ab43f44c3fb1bf7ac73e119e4c759da
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2025-05-10 18:59:03 +0200

    Ported from Android, 1

--- Sources/Core/GameChronometer.h
+++ Sources/Core/GameChronometer.h
@@ -7,4 +7,37 @@
 
 #include <chrono>
 
-using GameChronometer = std::chrono::steady_clock;
+/*
+ * A wall clock used to gather difference between two time points.
+ * It's independent of game state (paused vs. running).
+ */
+class GameChronometer final
+{
+public:
+
+    using time_point = std::chrono::steady_clock::time_point;
+    using duration = std::chrono::steady_clock::duration;
+
+    static time_point Now()
+    {
+        return std::chrono::steady_clock::now();
+    }
+
+    static float ElapsedSeconds(
+        time_point const & now,
+        time_point const & prev)
+    {
+        return std::chrono::duration_cast<std::chrono::duration<float>>(now - prev).count();
+    }
+
+    template<typename TDuration>
+    static float Progress(
+        time_point const & now,
+        time_point const & prev,
+        TDuration const & interval)
+    {
+        return
+            std::chrono::duration_cast<std::chrono::duration<float>>(now - prev).count()
+            / std::chrono::duration_cast<std::chrono::duration<float>>(interval).count();
+    }
+};
