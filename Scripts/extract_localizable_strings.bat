10c88e8607601d385347607cfc5868fabb1c9857
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2025-09-07 15:44:10 +0200

    Refreshed .pot; added two new ships

--- Scripts/extract_localizable_strings.bat
+++ Scripts/extract_localizable_strings.bat
@@ -1,2 +1,2 @@
 @echo off
-xgettext --c++ --from-code=cp1251 --keyword=_ --sort-output --no-wrap --omit-header --escape -o..\Localization\ui_strings.pot ..\FloatingSandbox\*.cpp ..\ShipBuilderLib\*.cpp ..\ShipBuilderLib\UI\*.cpp ..\ShipBuilder\*.cpp ..\UILib\*.cpp
+xgettext --c++ --from-code=cp1251 --keyword=_ --sort-output --no-wrap --omit-header --escape -o..\Localization\ui_strings.pot ..\Sources\FloatingSandbox\*.cpp ..\Sources\ShipBuilderLib\*.cpp ..\Sources\ShipBuilderLib\UI\*.cpp ..\Sources\ShipBuilder\*.cpp ..\Sources\UILib\*.cpp
