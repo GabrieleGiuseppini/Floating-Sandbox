d4a5ee40ff29c89905dbdccfebf87893b8455fe2
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2025-10-01 13:51:25 +0200

    Fixed problem with string extraction

--- Scripts/extract_localizable_strings.bat
+++ Scripts/extract_localizable_strings.bat
@@ -1,2 +1,2 @@
 @echo off
-xgettext --c++ --from-code=cp1251 --keyword=_ --sort-output --no-wrap --omit-header --escape -o..\Localization\ui_strings.pot ..\Sources\FloatingSandbox\*.cpp ..\Sources\ShipBuilderLib\*.cpp ..\Sources\ShipBuilderLib\UI\*.cpp ..\Sources\ShipBuilder\*.cpp ..\Sources\UILib\*.cpp
+xgettext --c++ --from-code=cp1251 --keyword=_ --sort-output --no-wrap --omit-header --escape -o..\Localization\ui_strings.pot ..\Sources\FloatingSandbox\*.cpp ..\Sources\FloatingSandbox\UI\*.cpp ..\Sources\ShipBuilderLib\*.cpp ..\Sources\ShipBuilderLib\UI\*.cpp ..\Sources\ShipBuilder\*.cpp ..\Sources\UILib\*.cpp
