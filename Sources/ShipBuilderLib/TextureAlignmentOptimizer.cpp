52402adbb6190181ba20744b2cbdccee11d90a09
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2025-11-12 22:10:03 +0100

    Fixed silly mistake

--- Sources/ShipBuilderLib/TextureAlignmentOptimizer.cpp
+++ Sources/ShipBuilderLib/TextureAlignmentOptimizer.cpp
@@ -26,13 +26,13 @@ RgbaImageData TextureAlignmentOptimizer::OptimizeAlignment(
 	//
 
 	std::vector<int> leftX;
-	leftX.reserve(source.Size.height);
+	leftX.reserve(source.Size.width);
 	std::vector<int> rightX;
-	rightX.reserve(source.Size.height);
+	rightX.reserve(source.Size.width);
 	std::vector<int> topY;
-	topY.reserve(source.Size.width);
+	topY.reserve(source.Size.height);
 	std::vector<int> bottomY;
-	bottomY.reserve(source.Size.width);
+	bottomY.reserve(source.Size.height);
 
 	CalculateEdges(source, leftX, rightX, topY, bottomY);
 
