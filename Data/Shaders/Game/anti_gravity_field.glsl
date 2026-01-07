efa4a7718480717652fb505c285b386c1a1ec9d7
Gabriele Giuseppini <gabriele.giuseppini@gmail.com>
2026-01-07 01:13:04 +0100

    Completed rendering

--- Data/Shaders/Game/anti_gravity_field.glsl
+++ Data/Shaders/Game/anti_gravity_field.glsl
@@ -38,9 +38,13 @@ uniform float paramSimulationTime;
 
 void main()
 {
-    // Max distance: 1.0
-    float d = length(vertexFieldSpaceCoords - vec2(0.5)) * 2.0;
-    float alpha = 1.0 - smoothstep(0.5, 1.0, d);
+    //
+    // Calculate shape
+    //
+
+    // Max distance: 0.5
+    float d = length(vertexFieldSpaceCoords - vec2(0.5));
+    float shapeAlpha = 1.0 - smoothstep(0.25, 0.5, d);
     
     //
     // Zero-centered y coords
@@ -61,25 +65,22 @@ void main()
         paramSimulationTime * PERTURBATION_SPEED);
     float perturbationNoise = texture2D(paramNoiseTexture, perturbationCoords).r; // 0..1
     float perturbationAngle = 0.8 * (perturbationNoise * 2.0 - 1.0);    
-    float vertexWorldXExtentPerturbated = vertexWorldXExtent + perturbationAngle * vertexFieldSpaceYCentered;
-    
+    float vertexWorldXExtentPerturbated = vertexWorldXExtent + perturbationAngle * vertexFieldSpaceYCentered;  
     
     //
     // Calculate noise coords
     //
     
-    #define H_RESOLUTION 50.0
+    #define H_RESOLUTION 0.02
     #define SPEED 1.0 / 30.0
+    #define NOISE_Y_FACTOR 0.15
     
-    float noiseYFactor = 0.15;
-    
-    float bottomHalfXOffset = step(vertexFieldSpaceCoords.y, 0.5) * 0.5;
-    float noiseY = vertexFieldSpaceYCentered * noiseYFactor;
+    float bottomHalfXOffset = step(vertexFieldSpaceCoords.y, 0.5) * 0.5; // We sample above differently than below
+    float noiseY = vertexFieldSpaceYCentered * NOISE_Y_FACTOR;
     float compressedNoiseY = smoothstep(0.0, 1.0, noiseY * 0.5) / 0.5;
-    //float compressedNoiseY = noiseY;
     
     vec2 noiseCoords = vec2(
-        vertexWorldXExtentPerturbated / H_RESOLUTION + bottomHalfXOffset,
+        vertexWorldXExtentPerturbated * H_RESOLUTION + bottomHalfXOffset,
         compressedNoiseY - paramSimulationTime * SPEED);
     float noise = texture2D(paramNoiseTexture, noiseCoords).x;
     
@@ -92,5 +93,5 @@ void main()
     ///////////////////////////////////////////////
     
     // glBlendFunc(GL_ONE, GL_ONE)
-    gl_FragColor = vec4(vec3(noise*alpha), 1.);
+    gl_FragColor = vec4(vec3(noise * shapeAlpha), 1.);
 }
